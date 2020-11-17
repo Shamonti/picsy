64e5b00f121d3af7b6033944b61da50b65915257
Shamonti <shamonti.haque98@gmail.com>
2020-11-17 23:04:55 +0600

    added carousel

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@
 $(function () {
   $(window).on('scroll', function () {
       if ( $(window).scrollTop() > 10 ) {
-          $('.navbar').addClass('scroll');
+        $('.navbar').addClass('scroll');
       } else {
-          $('.navbar').removeClass('scroll');
+        $('.navbar').removeClass('scroll');
       }
   });
 });
+
+
+  
